595932a9abb0210c61b475bdf84bf3ac25fba329
astroboy20 <tolulopeakinkunmi7@codenovation.org>
2025-01-02 15:04:11 +0100

    Learning web3 with solidity and react

---
+++ exercise-solidity.sol
@@ -0,0 +1,35 @@
+//this is a comment, this is where our code goes
+
+pragma solidity >=0.7.0 <0.9.0;
+
+//Task
+//1. Receive information
+//2. store information
+//3. return the informtion back
+
+//variable types and example in solidity
+    // uint256 storeData = 1;
+    // string name = "tom";
+    // bool switchOn = true;
+//function
+    // function functionName(){}
+
+
+//a simple storage contract
+contract simpleStorage {
+    // write all the code inside here - functions and its state
+    ///this is the state
+    uint storeData;
+   //a function to set  data
+   function set(uint x) public {
+    //create a storage contract that sets and gets values - only the values it reurns is multiplied 5x
+        storeData = x*5;
+   }
+   // a function to get data
+   function get() public view returns(uint)  {
+        return storeData;
+   }
+
+   
+   //create a storage contract that sets and gets values of NAMES
+}
